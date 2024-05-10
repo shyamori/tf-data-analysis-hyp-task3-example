63786a756057e1948579616115b4ffb79dc258e9
Maria <52673599+shyamori@users.noreply.github.com>
2024-05-10 18:18:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.stats import ttest_ind
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 386300009 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
