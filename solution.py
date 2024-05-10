410b841a072c95948ceb2fd5fd865c0940a8be3b
Maria <52673599+shyamori@users.noreply.github.com>
2024-05-10 18:13:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
@@ -8,4 +9,4 @@ def solution(...) -> bool: # Одна или две выборке на вход
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return ttest_ind(x, y, equal_var=False)[1] < 0.06
